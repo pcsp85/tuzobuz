bf939ee20b68ab13567be54c42aab1263e93c6e5
Pablo Sánchez <pablo.sanchez@seedgroup.com.mx>
2015-04-16 16:30:44 -0500

    avances 16-04-2015

--- app/app.js
+++ app/app.js
@@ -9,7 +9,41 @@ var tuzobusApp = angular.module('tuzobusApp',[
 
 tuzobusApp.config(['$routeProvider',
 	function ($routeProvider){
-		$routeProvider.when('/mapa',{
+		$routeProvider
+		.when('/',{
+			templateUrl: 'views/index.html',
+		})
+		.when('/mapa',{
 			templateUrl: 'views/mapa.html',
+		})
+		.when('/estaciones',{
+			templateUrl: 'views/estaciones.html',
+		})
+		.when('/servicios', {
+			templateUrl: 'views/servicios.html',
+		})
+		.when('/horarios', {
+			templateUrl: 'views/servicios.html',
+		})
+		.when('/comoUsar', {
+			templateUrl: 'views/comoUsar.html',
+		})
+		.when('/estacionesCercanas', {
+			templateUrl: 'views/estacionesCercanas.html',
+		})
+		.when('/rutasAlimentadoras', {
+			templateUrl: 'views/rutasAlimentadoras.html',
+		})
+		.when('/comoLlegar', {
+			templateUrl: 'views/comoLlegar.html',
+		})
+		.when('/notificaciones', {
+			templateUrl: 'views/notificaciones.html',
+		})
+		.when('/perfil', {
+			templateUrl: 'views/perfil.html',
+		})
+		.otherwise({
+			redirectTo: '/'
 		})
 	}]);
--- app/controllers.js
+++ app/controllers.js
@@ -4,8 +4,15 @@
 
 var tuzobusController = angular.module('tuzobusController',[]);
 
-tuzobusController.controller('tbMenuCtrl',['$scope', function ($scope){
+tuzobusController.controller('tbMenuCtrl',['$scope', '$http', function ($scope, $http){
+	
+	$scope.items = [];
+	$http.get('app/data/menu.json').success(function (data){
+		$scope.items = data;
+	});
+
 	$scope.hide = function (){
 		$('.navmenu-default').offcanvas('hide');
+		$('.navmenu-default a[href="#/'+this.item.target+'"]').addClass('active').parent().siblings().children('a').removeClass('active');
 	};
 }]);
